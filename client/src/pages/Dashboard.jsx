b528ec986796094904b8cdf91de5e8dfee32a312
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-04-16 21:31:48 +0200

    sidebar of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashProfile from '../components/DashProfile';
+import DashSideBar from '../components/DashSideBar';
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const tabFromUrl = searchParams.get('tab');
+
+    tabFromUrl && setTab(tabFromUrl);
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* sidebar */}
+        <DashSideBar />
+      </div>
+      {/* Profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 };
 
 export default Dashboard;
