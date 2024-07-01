455f1f3fffb52e86a5478c5e590adbe29ff8ea17
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-07-01 22:27:44 +0200

    update pre installation

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 
-app.use(express.static(path.join(__dirname, 'client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
