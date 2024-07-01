c7450babc9a337f4ad2ae173c95cd694290c7175
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-07-01 21:29:30 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import connectDb from './config/connectionDb.js';
 import authRoutes from './routes/auth.route.js';
 import commentRoutes from './routes/comment.route.js';
@@ -12,6 +13,8 @@ import userRoutes from './routes/user.route.js';
 dotenv.config();
 connectDb();
 const PORT = process.env.PORT || 5009
+
+const __dirname = path.resolve()
 
 const app = express();
 
@@ -32,6 +35,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
