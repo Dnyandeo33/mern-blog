877e59b2e76d3a8c55a79db7a1ff464cf14cb399
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-07-03 09:53:59 +0200

    bug fix node index js

--- api/index.js
+++ api/index.js
@@ -43,10 +43,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
 
